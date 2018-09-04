4637b2d4a55824e4b81b5f08afb321e9d0c6ffa1
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-09-04 17:32:28 -0300

    add new

---
+++ jacsmith.github.io/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  dest: 'vuepress',
+  serviceWorker: true,
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/page1',
+      '/page2',
+      [
+        '/page3',
+        'Custom link page'
+      ]
+    ]
+  },
+  title: 'Jacob Smith',
+  base: '/'
+}
